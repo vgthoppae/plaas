895700c2ae5ed5499aa28e73a55da4bd9ab2cf8d
vthoppae <venkat.thoppae@gmail.com>
2019-05-12 23:44:39 -0400

    adding app metadata

--- app-metadata/cheddar.groovy
+++ app-metadata/cheddar.groovy
@@ -3,7 +3,7 @@ import groovy.transform.Field
 @Field
 def cheddar = [:]
 
-cheddar.'name'= 'cheddar'
-cheddar.'gitrepo'= 'https://github.com/vgthoppae/plaas-cheddar-app.git'
+cheddar.name= 'cheddar'
+cheddar.gitrepo= 'https://github.com/vgthoppae/plaas-cheddar-app.git'
 
 return this
